9c58c7d428b682c1677f3e478035cc88fa30f01a
Анастасия Базанова <nastya.is.fine@mail.ru>
2022-06-02 23:48:32 +0300

    неработающая попытка

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int count = 0;
+        int i = 10;
+        int i <=99;
+        int x = i * i;
+        //диапазон от 300 до 400//
+
+        System.out.println("Количество подходящих чисел: " + count);
+    }
+}
---
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -0,0 +1,17 @@
+package ru.netology.sqr;
+
+public class SQRService {
+    public int getCount;
+    int count = 0;
+    for(
+    int i = 10;
+    i <=99;i++);
+
+    {
+        int x = i * i;
+        if (300 <= x && x <= 400) {
+            count++;
+        }
+        return count;
+    }
+}
